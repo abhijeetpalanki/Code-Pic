71e4f60935a88919ab931a8d9e0e49b5af002ff1
abhijeetpalanki <ajpalanki@gmail.com>
2024-06-10 10:41:12 -0500

    Language selector UI

--- app/components/LanguageSelector.tsx
+++ app/components/LanguageSelector.tsx
@@ -1,9 +1,61 @@
 "use client";
 
-import React from "react";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
+import { languages } from "../utils/utilities";
 
-const LanguageSelector = () => {
-  return <div>LanguageSelector</div>;
+interface LanguageSelectorProps {
+  language: string;
+  setLanguage: (language: string) => void;
+  setActiveIcon: (icon: string) => void;
+}
+
+const LanguageSelector = ({
+  language,
+  setLanguage,
+  setActiveIcon,
+}: LanguageSelectorProps) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleLanguageChange = (newLanguage: string) => {
+    setLanguage(newLanguage);
+    const newActiveIcon = languages.find(
+      (lang) => lang.name === newLanguage
+    )?.icon;
+
+    if (newActiveIcon) {
+      setActiveIcon(newActiveIcon);
+    }
+  };
+
+  return (
+    <div onClick={toggleDropdown}>
+      <p className="py-[5px] text-sm font-medium">Language</p>
+      <div className="capitalize w-[120px] pt-1 pr-[0.3rem] pb-[0.3rem] pl-2 h-[37px] rounded-[3px] border border-[rgba(249,249,249,0.08)] text-[0.9rem] flex items-center justify-between cursor-pointer">
+        {language}
+        <ChevronDown />
+      </div>
+
+      {showDropdown && (
+        <div className="p-[0.25rem_0_0.4rem_0.5rem] w-[120px] top-[94px] absolute bg-[#191919] border border-[#3c3c3c] rounded-[3px]">
+          {languages.map((lang, i) => (
+            <div key={i}>
+              <button
+                className="text-left"
+                onClick={() => handleLanguageChange(lang.name)}
+              >
+                {lang.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default LanguageSelector;
--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,19 @@ import LanguageSelector from "./components/LanguageSelector";
 
 export default function Home() {
   const [language, setLanguage] = useState(languages[0].name);
+  const [activeIcon, setActiveIcon] = useState(languages[0].icon);
 
   return (
     <main className="h-screen flex flex-col items-center justify-between">
       <header className="mt-6 w-[940px] fixed p-5 top-0 left-1/2 -translate-x-1/2 z-10 bg-[#191919] rounded border-[#3c3c3c] shadow-md">
-        <LanguageSelector />
+        <LanguageSelector
+          language={language}
+          setLanguage={setLanguage}
+          setActiveIcon={setActiveIcon}
+        />
       </header>
       <div className="code-editor-ref mt-56">
-        <CodeEditor language={language} />
+        <CodeEditor language={language} icon={activeIcon} />
       </div>
     </main>
   );
